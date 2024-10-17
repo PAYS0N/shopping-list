167d4b015277e84a7a81c890d4695a3ab3fa12fd
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-10-17 11:53:26 -0500

    Implement list element deletion

--- js/js.js
+++ js/js.js
@@ -1,13 +1,19 @@
+function removeItem() {
+    this.parentNode.remove();
+}
+
 function createItem() {
-    const item = document.createElement("div");
+    const item = document.createElement("li");
     const text = document.createElement("div");
     const buttonRemove = document.createElement("button");
     const input = document.querySelector(".input-text");
-    itemText = input.   value;
+    itemText = input.value;
+    input.value = '';
 
     text.textContent = itemText;
     buttonRemove.textContent = "Remove Item";
-    buttonRemove.setAttribute("class", "remove-button")
+    buttonRemove.setAttribute("class", "remove-button");
+    buttonRemove.addEventListener("click", removeItem);
 
     item.setAttribute("class", "item");
     item.appendChild(text)
@@ -15,6 +21,8 @@ function createItem() {
 
     const list = document.querySelector(".list");
     list.appendChild(item)
+
+    input.focus();
 }
 
 const buttonCreate = document.querySelector(".create");
